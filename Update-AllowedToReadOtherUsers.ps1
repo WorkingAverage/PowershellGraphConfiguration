c8fc3b895fe65518c8d86e639600a569445bc497
WorkingAverage <benjamin.tolar+wa@gmail.com>
2025-11-25 13:50:06 +0100

    Modified the file to be a function adding extra checks (Having the proper modules installed for the function to work)

--- Update-AllowedToReadOtherUsers.ps1
+++ Update-AllowedToReadOtherUsers.ps1
@@ -1,4 +1,5 @@
-<#
+function Update-AllowedToReadOtherUsers {
+    <#
 .SYNOPSIS
     Set the tenant setting "AllowedToReadOthers" to $true. Make sure you have the right permissions or to consent when the login windows pops-up.
 .DESCRIPTION
@@ -9,21 +10,33 @@
 .LINK
     https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.identity.signins/update-mgpolicyauthorizationpolicy
 .EXAMPLE
-   .\Update-AllowedToReadOtherUsers.ps1 -AllowedToReadOtherUsers:$true
+   Update-AllowedToReadOtherUsers.ps1 -AllowedToReadOtherUsers:$true
 #>
 
-param(
-    [Parameter(Mandatory = $true)]
-    [bool]$AllowedToReadOtherUsers
-)
-#Current Graph version 2.28.0
-#Permissions needed for updating the policies only
-Connect-MgGraph -Scopes "Policy.Read.All, Policy.ReadWrite.Authorization" -NoWelcome
-#(Find-MgGraphCommand Get-MgPolicyAuthorizationPolicy).permissions for confirming the authorization
-$policies = Get-MgPolicyAuthorizationPolicy
-$default_user_role_permissions = ($policies).DefaultUserRolePermissions
-#If you are using Microsoft Teams should keep this setting on $true, this will break the ability of users to add members to channels and such or see their names in the channels
-$default_user_role_permissions.AllowedToReadOtherUsers = $AllowedToReadOtherUsers
-#Remove the WhatIf to properly make the changes
-Update-MgPolicyAuthorizationPolicy -DefaultUserRolePermissions $default_user_role_permissions -Confirm:$true -WhatIf
+    param(
+        [Parameter(Mandatory = $true)]
+        [bool]$AllowedToReadOtherUsers
+    )
 
+    begin {
+        Import-Module "Microsoft.Graph.Identity.SignIns"
+        Import-Module "Microsoft.Graph.Authentication"
+    }
+
+    process {
+        #Current Graph version 2.28.0
+        #Permissions needed for updating the policies only
+        Connect-MgGraph -Scopes "Policy.Read.All, Policy.ReadWrite.Authorization" -NoWelcome
+        #(Find-MgGraphCommand Get-MgPolicyAuthorizationPolicy).permissions for confirming the authorization
+        $policies = Get-MgPolicyAuthorizationPolicy
+        $default_user_role_permissions = ($policies).DefaultUserRolePermissions
+        #If you are using Microsoft Teams should keep this setting on $true, this will break the ability of users to add members to channels and such or see their names in the channels
+        $default_user_role_permissions.AllowedToReadOtherUsers = $AllowedToReadOtherUsers
+        #Remove the WhatIf to properly make the changes
+        Update-MgPolicyAuthorizationPolicy -DefaultUserRolePermissions $default_user_role_permissions -Confirm:$true -WhatIf
+    }
+
+    end {
+        Disconnect-MgGraph | Out-Null
+    }
+}
