8172b3cfb54093738998634d9016b0610c49dfb8
WorkingAverage <benjamin.tolar+wa@gmail.com>
2025-12-09 14:55:23 +0100

    Get-DeviceLastSyncTime Updated the script to work properly with formatting the date so it can be more easily manipulated

--- Get-DeviceLastSyncTime.ps1
+++ Get-DeviceLastSyncTime.ps1
@@ -1,12 +1,18 @@
 function Get-DeviceLastSyncTime {
+    <#
+    .SYNOPSIS
+        Returns the last sync datetime value from Intune devices. Time will be converted to local machine time.
+    #>
+    
+    [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]$ComputerName
+        [Parameter(Mandatory = $true)][string[]]$ComputerName
     )
 
     begin {
         try {
             Import-Module -Name "Microsoft.Graph.Authentication"
-            Connect-MgGraph -Scopes Device.Read.All -NoWelcome -ContextScope Process
+            Connect-MgGraph -Scopes "Device.Read.All" -NoWelcome -ContextScope Process
         }
         catch {
             Write-Error $_
@@ -14,11 +20,11 @@ function Get-DeviceLastSyncTime {
     }
 
     process {
-        $devices = Invoke-MgGraphRequest -Method GET "https://graph.microsoft.com/v1.0/deviceManagement/manageddevices?`$select=devicename,lastsyncdatetime&`$filter=(contains(devicename,`'$computername`'))" -SkipHttpErrorCheck
-        $device = $devices.value | Where-Object { $_.devicename -eq $ComputerName }
-        $computername = $device.devicename
-        $lastsyncdatetime = $device.lastsyncdatetime.ToLocalTime()
-        return [PSCustomObject]@{"ComputerName" = $computername; "LastSyncDateTime" = $lastsyncdatetime }
+        $devices = [pscustomobject]@()
+        $devices = ($ComputerName | ForEach-Object { Invoke-MgGraphRequest -Method GET "https://graph.microsoft.com/v1.0/deviceManagement/manageddevices?`$select=devicename,lastSyncDateTime&`$filter=(contains(devicename,`'$_`'))" -SkipHttpErrorCheck }).value
+        $devices = $devices | ForEach-Object { [pscustomobject]$_ }
+        $devices | Where-Object { $_.lastsyncdatetime = $_.lastsyncdatetime.tolocaltime() }
+        return $devices
     }
 
     end {
